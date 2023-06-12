08080bd433a9c31e6bb3b1e7e4a7301b11342f7f
Khaled97 <khalilmaleki97@gmail.com>
2023-06-12 15:53:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react";
+
+function App(){
+  return (
+    <div className="App">
+      const [yourName] = useState("Khalil");
+      <h1>Hello, This is my first React App</h1>
+    </div>
+
+  );
+}
+
+export default App;
